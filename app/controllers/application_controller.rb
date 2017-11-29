871e56bfd9c8bd9fef562f3421964d99664e2a78
Jason Rae <jrae@wearefuturegov.com>
2017-11-29 15:55:16 +0000

    Adding after sign_in_path for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource_or_scope)
+    if current_user.solution
+      edit_solution_path(current_user.solution)
+    else
+      :new_solution
+    end
+  end
 end
