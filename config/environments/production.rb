4ffca62321e1c01d9925d6fc96192300540e0845
Jason Rae <jrae@wearefuturegov.com>
2017-12-01 16:55:42 +0000

    Add sendgrid settings for email notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ GizSolutions::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
 end
