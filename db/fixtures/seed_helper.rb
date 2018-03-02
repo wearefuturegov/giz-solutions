3721e3f08797d9bbd0c87c69f6222d6f035b2292
Jason Rae <jrae@wearefuturegov.com>
2018-03-02 07:38:45 -0800

    Fix seeding to account for solutions need terms accepting

--- db/fixtures/seed_helper.rb
+++ db/fixtures/seed_helper.rb
@@ -7,7 +7,7 @@ class SeedHelper
     count.times.each do |n|
       email_address = "test-account#{n}@example.com"
       next if User.where(email: email_address).any?
-      create_user(email_address)
+      create_solution(email_address)
     end
   end
 
@@ -23,24 +23,25 @@ class SeedHelper
     end
   end
 
-  def create_user(email_address)
-    User.new(email: email_address,
-             first_name: FFaker::Name.first_name,
-             last_name: FFaker::Name.last_name,
-             confirmed_at: Time.zone.now,
-             password: ENV['DEFAULT_PASSWORD'],
-             solution: new_solution).save!
-  end
-
-  def new_solution
-    Solution.new(website: 'https://giz-solutions.wearefuturegov.com/',
+  def create_solution(email)
+    Solution.new(user: new_user(email),
+                 website: 'https://giz-solutions.wearefuturegov.com/',
                  title: FFaker::BaconIpsum.sentence,
                  description: FFaker::BaconIpsum.paragraph,
                  business_opportunities: FFaker::BaconIpsum.paragraph,
                  implementation_impact: FFaker::BaconIpsum.paragraph,
                  skills_experience: FFaker::BaconIpsum.paragraph,
+                 terms_accepted: true,
                  hero_image: File.new(
                    Rails.root.join('app', 'assets', 'images', 'cats', "#{rand(1...10)}.jpeg")
-                 ))
+                 )).save!
+  end
+
+  def new_user(email_address)
+    User.new(email: email_address,
+             first_name: FFaker::Name.first_name,
+             last_name: FFaker::Name.last_name,
+             confirmed_at: Time.zone.now,
+             password: ENV['DEFAULT_PASSWORD'])
   end
 end
