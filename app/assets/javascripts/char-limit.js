12cc53d7edeb18711e2d2115f864103f97c25d29
Chris Evans <evanschris@users.noreply.github.com>
2018-03-14 17:16:08 +0000

    add in remaining char count before content is edited

--- app/assets/javascripts/char-limit.js
+++ app/assets/javascripts/char-limit.js
@@ -1,5 +1,12 @@
 $.fn.handleCharCount = function() {
 
+  $(this).find("textarea").each(function() {
+    $(this).siblings("span.char_count").text("Remaining characters: " + (parseInt($(this).attr('maxlength')) - $(this).val().length));
+  });
+  $(this).find("input").each(function() {
+    $(this).siblings("span.char_count").text("Remaining characters: " + (parseInt($(this).attr('maxlength')) - $(this).val().length));
+  });
+
   $(this).find("textarea").bind('change keyup paste', textareaChange);
   $(this).find("input").bind('change keyup paste', textareaChange);
 
