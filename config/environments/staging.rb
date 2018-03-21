a8c43bb19c8acba41251251d90bb182fc67abe0b
Jason Rae <jrae@wearefuturegov.com>
2018-03-21 14:42:30 -0700

    Use herokuapp.com domain rather than heroku to ensure email routes faster

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -75,7 +75,7 @@ GizSolutions::Application.configure do
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    domain: 'herokuapp.com',
     enable_starttls_auto: true
   }
 end
