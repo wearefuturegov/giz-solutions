e4de101fcdd85e6e65a6cc707f4eb0e4fb03df80
Jason Rae <jrae@wearefuturegov.com>
2018-03-22 11:10:19 -0700

    Although we are not using CustomDeviseMailer I’m checking in anyway in case we want to revisit
    the code works just this issue was being a pain and using the
    example.org
    
    https://github.com/rails/rails/issues/31979

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,16 +65,14 @@ GizSolutions::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { host: ENV['HOST_URL'] }
-
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-     :address => 'localhost',
-     :port => '25',
-     :domain => 'localhost.localdomain',
-     :enable_starttls_auto => false
-   }
+    address: 'localhost',
+    port: '25',
+    domain: 'localhost.localdomain',
+    enable_starttls_auto: false
+  }
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,10 +14,10 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@example.com'
+  config.mailer_sender = GizSolutions.config.support_email
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  # config.mailer = CustomDeviseMailer
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
