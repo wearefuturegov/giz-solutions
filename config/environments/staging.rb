2618607ae227ad434a8875496e095b5630f8de07
Jason Rae <jrae@wearefuturegov.com>
2018-03-21 22:39:06 -0700

    Try removing action mailer config and just use send grid-ruby

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -67,15 +67,15 @@ GizSolutions::Application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['HOST_URL'] }
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'herokuapp.com',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.sendgrid.net',
+  #   port: '587',
+  #   authentication: :plain,
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: 'herokuapp.com',
+  #   enable_starttls_auto: true
+  # }
 end
