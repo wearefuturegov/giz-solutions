1e88255ae3542d6b34939115d63a5400d42325bd
Jason Rae <jrae@wearefuturegov.com>
2018-03-22 18:22:57 -0700

    Try and initialise the SDK in the way we do for Local Offer

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -1,17 +1,16 @@
-// window.fbAsyncInit = function() {
-//   FB.init({
-//     appId      : _FACEBOOK_ID,
-//     xfbml      : true,
-//     autoLogAppEvents : true,
-//     version    : 'v2.10'
-//   });
-//   FB.AppEvents.logPageView();
-// };
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : _FACEBOOK_ID,
+    autoLogAppEvents : false,
+    xfbml            : true,
+    version          : 'v2.12'
+  });
+};
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=' + _FACEBOOK_ID;
-  fjs.parentNode.insertBefore(js, fjs);
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
