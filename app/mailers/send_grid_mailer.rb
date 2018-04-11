f3de67988b5818d4e14e3d96f005cf584c0580d1
Jason Rae <jrae@wearefuturegov.com>
2018-04-11 10:13:01 -0700

    Try using an ASM group which can be used to unsubscribe
    Temporarily adding a load of logging in there too

--- app/mailers/send_grid_mailer.rb
+++ app/mailers/send_grid_mailer.rb
@@ -18,10 +18,12 @@ module SendGridMailer
 
   def sendgrid_mail(subject, to, content)
     mail = Mail.new(from_email, subject, to, content)
-    response = sendgrid_client.mail._('send').post(request_body: mail.to_json)
-    # puts response.status_code
-    # puts response.body
-    # puts response.headers
+    mail.asm = ASM.new(group_id: 5827)
+    puts request_body = mail.to_json
+    response = sendgrid_client.mail._('send').post(request_body: request_body)
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 
   def sendgrid_client
--- app/mailers/solution_mailer.rb
+++ app/mailers/solution_mailer.rb
@@ -4,10 +4,14 @@ class SolutionMailer < ActionMailer::Base
   include SendGridMailer
 
   def notify_admins(admin_emails, new_solution)
-    sendgrid_mail(
-      I18n.t('mail.solution.subject.new_solution'),
-      admin_emails.collect { |email| Email.new(email: email) },
-      get_content('solution_mailer/notify_admins', new_solution: new_solution)
-    )
+    admin_emails.each do |email|
+      if email == 'jrae@wearefuturegov.com'
+        sendgrid_mail(
+          I18n.t('mail.solution.subject.new_solution'),
+          Email.new(email: email),
+          get_content('solution_mailer/notify_admins', new_solution: new_solution)
+        )
+      end
+    end
   end
 end
