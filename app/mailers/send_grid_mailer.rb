de550570a55e6f2abad4ff1cc84e8f3a88a2b213
Jason Rae <jrae@wearefuturegov.com>
2018-04-11 10:13:01 -0700

    Using an ASM group which can be used to unsubscribe
    TODO customise in a generic way

--- app/mailers/send_grid_mailer.rb
+++ app/mailers/send_grid_mailer.rb
@@ -18,6 +18,8 @@ module SendGridMailer
 
   def sendgrid_mail(subject, to, content)
     mail = Mail.new(from_email, subject, to, content)
+    mail.asm = ASM.new(group_id: 5827)
+    # puts request_body = mail.to_json
     response = sendgrid_client.mail._('send').post(request_body: mail.to_json)
     # puts response.status_code
     # puts response.body
@@ -29,6 +31,6 @@ module SendGridMailer
   end
 
   def from_email
-    Email.new(email: GizSolutions.config.support_email)
+    @from ||= Email.new(email: GizSolutions.config.support_email)
   end
 end
--- app/mailers/solution_mailer.rb
+++ app/mailers/solution_mailer.rb
@@ -4,10 +4,12 @@ class SolutionMailer < ActionMailer::Base
   include SendGridMailer
 
   def notify_admins(admin_emails, new_solution)
-    sendgrid_mail(
-      I18n.t('mail.solution.subject.new_solution'),
-      admin_emails.collect { |email| Email.new(email: email) },
-      get_content('solution_mailer/notify_admins', new_solution: new_solution)
-    )
+    admin_emails.each do |email|
+      sendgrid_mail(
+        I18n.t('mail.solution.subject.new_solution'),
+        Email.new(email: email),
+        get_content('solution_mailer/notify_admins', new_solution: new_solution)
+      )
+    end
   end
 end
