1953ac390a79eb54f9186da163dc1ef10c98aecc
Chris Evans <evanschris@users.noreply.github.com>
2018-05-09 10:56:19 +0100

    test new font asset pipeline for heroku

--- config/application.rb
+++ config/application.rb
@@ -101,5 +101,7 @@ module GizSolutions
 
     # config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile += %w[facebook.js]
+
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
