14122ac4c5aba7c319b2b0fd26a79c733242a348
pezholio <pezholio@gmail.com>
2018-07-30 14:30:46 +0100

    Bust cache when creating new solution
    So going back will automatically refresh the page

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -6,6 +6,8 @@ class SolutionsController < ApplicationController
   before_action :authenticate_can_edit!, only: %i[update edit]
 
   before_action :authenticate_can_show_archived!, only: %i[show]
+  before_action :set_cache_headers, only: %i[new]
+  
   expose :solution, scope: :with_deleted
 
   expose :winning_solutions, -> { Solution.winners }
@@ -76,6 +78,12 @@ class SolutionsController < ApplicationController
   end
 
   private
+  
+  def set_cache_headers
+    response.headers['Cache-Control'] = 'no-cache, no-store'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
+  end
 
   def authenticate_can_show_archived!
     return unless solution.deleted?
